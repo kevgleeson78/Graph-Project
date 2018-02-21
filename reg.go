2a6b29c673f2b4137d840562ff1f8edf6e370fd2
Kevin <Kevin@DESKTOP-12A507O>
2018-02-21 20:12:07 +0000

    test inputs setup for postfix function

--- reg.go
+++ reg.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+func intoPost(infix string) string {
+	posfix := ""
+
+	return posfix
+}
 func main() {
-	fmt.Println("Test...")
+
+	fmt.Println("infix: ", "a.b.c*")
+	fmt.Println("PostFix: ", intoPost("a.b.c*"))
 }
