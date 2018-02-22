2cae29d23974e7627e6f2bf4e6683cb0e7c7fd73
Kevin <Kevin@DESKTOP-12A507O>
2018-02-22 21:14:21 +0000

    Test environment setup

--- reg.go
+++ reg.go
@@ -10,7 +10,17 @@ func intoPost(infix string) string {
 	return posfix
 }
 func main() {
-
+	//Answer ab.c*
 	fmt.Println("infix: ", "a.b.c*")
 	fmt.Println("PostFix: ", intoPost("a.b.c*"))
+	//Answer abd|.*
+	fmt.Println("infix: ", "(a.(b|d))*")
+	fmt.Println("PostFix: ", intoPost("(a.(b|d))*"))
+	//Answer abd|.c
+	fmt.Println("infix: ", "a.(b|d).c")
+	fmt.Println("PostFix: ", intoPost("a.(b|d).c"))
+	//Answer abb.+.c.
+	fmt.Println("infix: ", "a.(b.b)+.c")
+	fmt.Println("PostFix: ", intoPost("a.(b.b)+.c"))
+
 }
