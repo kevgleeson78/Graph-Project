e335211aa05b672ae99c794ce634d455334a6d56
Kevin <Kevin@DESKTOP-12A507O>
2018-04-01 14:22:31 +0100

    function steup for regular expression and output string to match

--- nfa.go
+++ nfa.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type state struct {
 	symbol rune
 	edge1  *state
@@ -10,10 +14,65 @@ type nfa struct {
 	accept  *state
 }
 
-func posRegNfa(posfix string) {
+func posRegNfa(posFix string) *nfa {
+	nfaStack := []*nfa{}
+	for _, r := range posFix {
+		switch r {
+		case '.':
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
 
+			frag1.accept.edge1 = frag2.initial
+			nfaStack = append(nfaStack, &nfa{initial: frag2.initial, accept: frag2.accept})
+
+		case '|':
+			frag2 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			frag1 := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			initial := state{edge1: frag1.initial, edge2: frag2.initial}
+			accept := state{}
+			frag1.accept.edge1 = &accept
+			frag2.accept.edge1 = &accept
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		case '*':
+			frag := nfaStack[len(nfaStack)-1]
+			nfaStack = nfaStack[:len(nfaStack)-1]
+			accept := state{}
+			initial := state{edge1: frag.initial, edge2: &accept}
+			frag.accept.edge1 = frag.initial
+			frag.accept.edge2 = &accept
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		default:
+			accept := state{}
+			initial := state{symbol: r, edge1: &accept}
+			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		}
+	}
+	if len(nfaStack) != 1 {
+		fmt.Println("Uh Oh:", len(nfaStack), nfaStack)
+	}
+	return nfaStack[0]
 }
+func pomatch(po string, s string) bool {
+	ismatch := false
+	ponfa := posRegNfa(po)
 
+	current := []*state{}
+	next := []*state{}
+	for _, r := range s {
+		for _, c := range current {
+
+		}
+		current, next = next, []*state{}
+	}
+
+	return ismatch
+}
 func main() {
+	fmt.Println(pomatch("ab.c*|", "ccc"))
 	nfa := posRegNfa("ab.c*|")
+	fmt.Println(nfa)
 }
