7caf2a028b62cfc7bfbe25574199bb25908ca65f
Kevin <Kevin@DESKTOP-12A507O>
2018-04-07 15:18:00 +0100

    Comments added to reg.go

--- reg.go
+++ reg.go
@@ -34,20 +34,25 @@ func IntoPost(infix string) string {
 				//select and store in s everything apart from the last rune in the array s.
 				s = s[:len(s)-1]
 			}
-			//remove open bracket
+			//remove open bracket by gettint everything in s array apart from the last character.
 			s = s[:len(s)-1]
-			//
+			//case for mapped runes greater than 0. 0 in runes are equal to null.
 		case specials[r] > 0:
+			//loop while s greater than 0 and the value of the special rune is less than the value of the character of the top of the stack.
 			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				//append each rune to the end of the array posfix
 				posfix = append(posfix, s[len(s)-1])
+				//select and store in s everything apart from the last rune in the array s.
 				s = s[:len(s)-1]
 			}
+			//Append s from stack to s
 			s = append(s, r)
 		default:
 			posfix = append(posfix, r)
 		}
 	}
 	for len(s) > 0 {
+		//Append anything that is left in the stack.
 		posfix = append(posfix, s[len(s)-1])
 		s = s[:len(s)-1]
 	}
@@ -56,10 +61,11 @@ func IntoPost(infix string) string {
 }
 
 /*
-	//Answer ab.c*.
+	//Answer ab.c*. => ('a' followed by a 'b' followed by 0 or more 'c')
 	fmt.Println("infix: ", "a.b.c*")
 	fmt.Println("PostFix: ", intoPost("a.b.c*"))
-	//Answer abd|.*
+
+	//Answer abd|.* => ()
 	fmt.Println("infix: ", "(a.(b|d))*")
 	fmt.Println("PostFix: ", intoPost("(a.(b|d))*"))
 	//Answer abd|.c*.
