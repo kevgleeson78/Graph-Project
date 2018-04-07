8ce8e1542eebe1b06d4ff2907eb260328cd39176
Kevin <Kevin@DESKTOP-12A507O>
2018-04-07 11:23:51 +0100

    Some comments added.

--- nfa.go
+++ nfa.go
@@ -1,6 +1,7 @@
 /*
 *App Name: Gmit-Project
 *Author:Kevin Gleeson
+*Student Number: G00353180
 *Version: 1.0
 *Resources:
 *
@@ -28,28 +29,44 @@ type nfa struct {
 
 //Function to take in a postfix string as a param and return a pointer to nfa struct
 func posRegNfa(posFix string) *nfa {
+	//Array Stack to hold empty nfa pointers
 	nfaStack := []*nfa{}
+	//loop over each rune from posFix String input
 	for _, r := range posFix {
+		//switch statement for specila characters in the string
 		switch r {
+		//Concatination character
 		case '.':
+			//get the last character in the nfaStack array
 			frag2 := nfaStack[len(nfaStack)-1]
+			//get everything up to the last character in the nfaStack array
 			nfaStack = nfaStack[:len(nfaStack)-1]
+			//Get the last character from the above newly assigned nfaStack
 			frag1 := nfaStack[len(nfaStack)-1]
+			//get everything up to the last element in the array
 			nfaStack = nfaStack[:len(nfaStack)-1]
-
+			//Joining the two fragments together
+			//frag1 first edge points to frag2 initial state.
 			frag1.accept.edge1 = frag2.initial
+			//append a new pointer to nfaStack of frag1 initial state and frag2 accept state.
 			nfaStack = append(nfaStack, &nfa{initial: frag2.initial, accept: frag2.accept})
-
+			//Union Character
 		case '|':
+			//get the last character in the nfaStack array
 			frag2 := nfaStack[len(nfaStack)-1]
+			//get everything up to the last character in the nfaStack array
 			nfaStack = nfaStack[:len(nfaStack)-1]
+			//Get the last character from the above newly assigned nfaStack
 			frag1 := nfaStack[len(nfaStack)-1]
+			//get everything up to the last element in the array
 			nfaStack = nfaStack[:len(nfaStack)-1]
+
 			initial := state{edge1: frag1.initial, edge2: frag2.initial}
 			accept := state{}
 			frag1.accept.edge1 = &accept
 			frag2.accept.edge1 = &accept
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+			//Klanee star character
 		case '*':
 			frag := nfaStack[len(nfaStack)-1]
 			nfaStack = nfaStack[:len(nfaStack)-1]
@@ -58,15 +75,18 @@ func posRegNfa(posFix string) *nfa {
 			frag.accept.edge1 = frag.initial
 			frag.accept.edge2 = &accept
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
+		//All other characters
 		default:
 			accept := state{}
 			initial := state{symbol: r, edge1: &accept}
 			nfaStack = append(nfaStack, &nfa{initial: &initial, accept: &accept})
 		}
 	}
+	//Error check that there is only one element left on the stack
 	if len(nfaStack) != 1 {
 		fmt.Println("Uh Oh:", len(nfaStack), nfaStack)
 	}
+	//Return the last nfa on the stack
 	return nfaStack[0]
 }
 
